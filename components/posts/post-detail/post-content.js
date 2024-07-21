26ce491c9e1ff00f9b78b1040ad46c36560d311f
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2024-07-21 10:37:12 +0300

    contact and posts updated

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,27 +1,55 @@
-
-
+import Image from "next/image";
 import Markdown from "react-markdown";
-import classes from "./post-content.module.css"
+import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-const DUMMY_POST = {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-    content: "# This is a first post",
+const PostContent = (props) => {
+  const { content, image, slug, title } = props.post;
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+              priority={true}
+              
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
   };
 
-const PostContent = () => {
-    const {content,date,image,slug,title} = DUMMY_POST;
-
-    const imagePath=`/images/posts/${slug}/${image}`
-    return (
-        <article className={classes.content}>
-            <PostHeader title={title} image={imagePath}/>
-            <Markdown>{content}</Markdown>
-        </article>
-    );
+  return (
+    <article className={classes.content}>
+      <PostHeader title={title} image={imagePath} />
+      <Markdown components={customRenderers}>{content}</Markdown>
+    </article>
+  );
 };
 
 export default PostContent;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,22 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/post-utils";
 
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with NextJS",
-    image:"getting-started-nextjs.png",
-    excerpt:"NextJs is a react framework for production- it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date :"2022-02-10",
-  },{
-    slug: "getting-started-with-nextjs2",
-    title: "Getting started with NextJS",
-    image:"getting-started-nextjs.png",
-    excerpt:"NextJs is a react framework for production- it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date :"2022-02-10",
-  },{
-    slug: "getting-started-with-nextjs3",
-    title: "Getting started with NextJS",
-    image:"getting-started-nextjs.png",
-    excerpt:"NextJs is a react framework for production- it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date :"2022-02-10",
-  },{
-    slug: "getting-started-with-nextjs4",
-    title: "Getting started with NextJS",
-    image:"getting-started-nextjs.png",
-    excerpt:"NextJs is a react framework for production- it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date :"2022-02-10",
-  },
-];
 
-const AllPostsPage = ()=> {
+
+const AllPostsPage = (props)=> {
     return (
-      <AllPosts posts={DUMMY_POSTS}/>
+      <AllPosts posts={props.posts}/>
     )
   }
 
-  
+export const getStaticProps=()=>{
+  const allPosts = getAllPosts();
+  return {
+    props:{
+      posts:allPosts
+    }
+  }
+}
   
   export default AllPostsPage;
