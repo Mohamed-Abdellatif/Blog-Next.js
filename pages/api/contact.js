0aaf3c5777833c0a14f082f354c84e579a469169
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2024-07-23 13:36:06 +0300

    adding next config file

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,50 +1,57 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const MONGODB_URI =
-  "mongodb+srv://mabdellatif411:sWVnxjNU9YN2Mwnw@cluster0.hybw0wo.mongodb.net/my-blog?retryWrites=true&w=majority&appName=Cluster0";
 
-const handler = async (req, res) => {
-  if (req.method === "POST") {
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
     const { email, name, message } = req.body;
+
     if (
       !email ||
-      !email.includes("@") ||
+      !email.includes('@') ||
       !name ||
-      name.trim() === "" ||
+      name.trim() === '' ||
       !message ||
-      message.trim() === ""
+      message.trim() === ''
     ) {
-      res.status(422).json({ message: "Invalid input" });
+      res.status(422).json({ message: 'Invalid input.' });
       return;
     }
+
     const newMessage = {
       email,
       name,
       message,
     };
+
     let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.hybw0wo.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority&appName=Cluster0`;
+    
     try {
-      client = await MongoClient.connect(MONGODB_URI);
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
-      res.status(501).json({ message: "Could not connect to database" });
+      res.status(500).json({ message: 'Could not connect to database.' });
       return;
     }
+
     const db = client.db();
 
     try {
-      const result = await db.collection("messages").insertOne(newMessage);
+      const result = await db.collection('messages').insertOne(newMessage);
       newMessage.id = result.insertedId;
     } catch (error) {
-      res.status(500).json({ message: "Storing message failed" });
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
     }
 
     client.close();
 
-    res.status(201).json({
-      message: "Successfully stored message!",
-      message: newMessage,
-    });
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
   }
-};
+}
 
 export default handler;
