314bd3d88ba7526c8cf73e08ebf4aa4199bec3bd
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2024-07-23 13:53:46 +0300

    debugging

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -30,7 +30,7 @@ const ContactForm = () => {
       const timer = setTimeout(()=>{
         setRequestStatus(null);
         setRequestError(null)
-      },3000)
+      },100000)
 
       return ()=> clearTimeout(timer)
     }
@@ -80,7 +80,7 @@ const ContactForm = () => {
       message: requestError,
     };
   }
-  console.log(requestError)
+  
   return (
     <section className={classes.contact}>
       <h1>How Can I Help You?</h1>
